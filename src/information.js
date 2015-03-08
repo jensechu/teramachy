dc264b70ae329b88129ff63bb1a4348438cc2f09
Jensen <jensechu@gmail.com>
2015-03-08 13:41:47 -0700

    :partly_sunny: Create a simple static monster

---
+++ src/information.js
@@ -0,0 +1,50 @@
+var Information = React.createClass({
+    propTypes: {
+      species: React.PropTypes.array
+    },
+    mixins : [],
+
+    getInitialState: function() {
+      return {};
+    },
+    getDefaultProps: function() {
+      return {
+        species: ['kitty', 'dog', 'mouse']
+      };
+    },
+
+    componentWillMount : function() {},
+    componentWillReceiveProps: function() {},
+    componentWillUnmount : function() {},
+
+    _parseData : function() {},
+    _onSelect : function() {},
+
+    render : function() {
+      var species = this.props.species.map(function(species, i) {
+        //Maybe these options should be it's own component?
+        return (
+          <option>{species}</option>
+        );
+      }.bind(this))
+      return (
+        <form name='monsterInformation'>
+          <input
+            ref='name'
+            type='text'
+            onChange={this.props.update}
+          />
+
+          <select
+            ref='species'
+            onChange={this.props.update}>
+            {species}
+
+          </select>
+
+          <button onClick={this.props.handleCreation}>Create</button>
+        </form>
+      );
+    }
+
+});
--- src/monster.js
+++ src/monster.js
@@ -1,14 +1,27 @@
 var Monster = React.createClass({
     propTypes: {
       name: React.PropTypes.string,
-      src: React.PropTypes.string
+      species: React.PropTypes.string,
+      hunger: React.PropTypes.number
     },
     mixins : [],
 
     getInitialState: function() {
-      return {};
+      return {
+        name: '',
+        species: 'egg',
+        hunger: 0
+      };
     },
-    getDefaultProps: function() {},
+
+    handleCreation: function(e) {
+      e.preventDefault();
+
+      this.setState({
+        name: this.refs.information.refs.name.getDOMNode().value,
+        species: this.refs.information.refs.species.getDOMNode().value
+      });
+    },
 
     componentWillMount : function() {},
     componentWillReceiveProps: function() {},
@@ -17,11 +30,18 @@ var Monster = React.createClass({
     _parseData : function() {},
     _onSelect : function() {},
 
+    _formattedImageURL: function(string) {
+      return './assets/images/' + string + '.png';
+    },
+
     render : function() {
       return (
         <div className='monster'>
-          <img src={this.props.src}></img>
-          <h2>My name is {this.props.name}!</h2>
+          <img src={this._formattedImageURL(this.state.species)}></img>
+          <h2>Name: {this.state.name}</h2>
+          <h2>Species: {this.state.species}</h2>
+
+          <Information ref='information' handleCreation={this.handleCreation} />
         </div>
       );
     }
